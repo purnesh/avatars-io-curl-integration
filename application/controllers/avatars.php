74037c9d05426591aefd1fa3b3ce5ead037765fb
purnesh <purnesh.xyz@gmail.com>
2014-10-31 11:35:00 +0530

    Seems like instagram doesn't support different sizes of avatars!

--- application/controllers/avatars.php
+++ application/controllers/avatars.php
@@ -2,7 +2,7 @@
 	class Avatars extends CI_Controller{
 		public function index(){
 			$this->load->model('Avatars_model', 'avatars');
-			$url =  $this->avatars->instagram('purneshtripathi');
+			$url =  $this->avatars->sized('instagram', 'purneshtripathi', 'big');
 			echo "<img src='$url' />";
 		}
 	}
